bc32706eb708fd112e8f94b3b7fd801813e41faa
CodeLuisss <luishenrique.mendes2004@gmail.com>
2025-06-09 23:16:23 -0300

    este é um commit

---
+++ aprendendo.py
@@ -0,0 +1,14 @@
+salario = 1412
+
+aumento = 100.50
+
+novo_salario = salario + aumento
+print(novo_salario)
+
+###################################################
+
+velocidade = 80
+
+limite = 60
+
+print(velocidade < limite)
